50f125cc7e42a67450ed8399a07249ef88f89445
Valentin Deville <contact@valentin-deville.eu>
2017-04-30 11:56:28 +0200

    Modify bot.php

--- bot.php
+++ bot.php
@@ -12,6 +12,8 @@ require_once 'vendor/autoload.php';
 DEFINE('USER', '');
 DEFINE('OAUTH', 'oauth:');
 DEFINE('IRC_ADDRESS', 'irc.chat.twitch.tv');
+DEFINE('PORT', 6667);
+DEFINE('CHANNEL', 'v_deville');
 
-$connect = new IrcConnect(IRC_ADDRESS, 6667, USER,'v_deville',OAUTH);
+$connect = new IrcConnect(IRC_ADDRESS, PORT, USER,CHANNEL,OAUTH);
 $connect->launch($connect->connect());
