a56d5731784d209eae565ef22f38ec3d18c7fd8d
Valentin Deville <contact@valentin-deville.eu>
2017-04-30 23:55:13 +0200

    Add meblock module for block /me command

---
+++ modules/meblock/Meblock.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Class Meblock
+ */
+class Meblock {
+
+    use \TwitchBot\Module;
+
+    /**
+     * Meblock constructor.
+     * @param array $infos
+     * @param \TwitchBot\IrcConnect $client
+     */
+    public function __construct(array $infos, $client)
+    {
+        $this->client = $client;
+        $this->infos = $infos;
+    }
+
+    /**
+     * @param \TwitchBot\Message $data
+     */
+    public function onMessage($data)
+    {
+        if(preg_match("/ACTION/", $data->getOriginalMsg()) && $data->getUserType() < 2){
+            $this->timeout($data->getUsername(), 20);
+            $this->getClient()->sendMessage('@' . $data->getUsername() . ', usage of /me is not permit here.');
+        }
+    }
+
+    /**
+     * @param $user
+     * @param $time
+     */
+    private function timeout($user, $time)
+    {
+        $this->getClient()->sendMessage('.timeout ' . $user . ' ' . $time);
+        $this->getClient()->sendToLog('User ' . $user . ' tiemout ' . $time);
+    }
+}
