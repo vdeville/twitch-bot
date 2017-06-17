b8fecf95f38342b5999b80d2826774c433d756fe
Valentin Deville <contact@valentin-deville.eu>
2017-06-17 14:38:31 +0200

    Add optional delay to prevent commands spam

--- modules/commands/Commands.php
+++ modules/commands/Commands.php
@@ -9,6 +9,10 @@ class Commands {
 
     private $commands;
 
+    private static $DELAY = 30;
+
+    private $lastCommands;
+
     /**
      * Commands constructor.
      * @param array $infos
@@ -19,6 +23,8 @@ class Commands {
         $this->commands = json_decode(file_get_contents(__DIR__ . '/commands.json'), true);
         $this->client = $client;
         $this->infos = $infos;
+
+        $this->lastCommands = [];
     }
 
     public function onConnect()
@@ -40,11 +46,7 @@ class Commands {
             $userToPing = (isset($explode[1])) ? $explode[1] : false;
 
             if(key_exists($command, $this->getCommands())){
-                if($userToPing != false){
-                    $this->getClient()->sendMessage($userToPing . ', ' . $this->getCommands($command));
-                } else {
-                    $this->getClient()->sendMessage($this->getCommands($command));
-                }
+                $this->sendResponse($command, $userToPing);
             } else{
                 $this->getClient()->sendToLog('The command !' . $command . ' is not mapped');
             }
@@ -66,6 +68,33 @@ class Commands {
         } else {
             return $this->commands;
         }
+    }
+
+    /**
+     * @param $command
+     * @param $userToPing
+     */
+    public function sendResponse($command, $userToPing){
+
+        if(isset($this->lastCommands[$command])){
+            $time = $this->lastCommands[$command];
+        } else {
+            $time = time() - self::$DELAY;
+        }
+
+        $diff = time() - $time;
+
+        if($diff >= self::$DELAY){
+
+            if($userToPing != false){
+                $this->getClient()->sendMessage($userToPing . ', ' . $this->getCommands($command));
+            } else {
+                $this->getClient()->sendMessage($this->getCommands($command));
+            }
+
+            $this->lastCommands[$command] = time();
+        }
+
     }
 
 }
