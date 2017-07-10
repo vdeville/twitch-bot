161362e16bb2a18d495814d799501f58cb0c4b07
Valentin Deville <contact@valentin-deville.eu>
2017-07-10 21:53:30 +0200

    Add Googleit module

---
+++ modules/googleit/Googleit.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Class Googleit
+ */
+class Googleit
+{
+    use \TwitchBot\Module {
+        \TwitchBot\Module::__construct as private moduleConstructor;
+    }
+
+    /**
+     * Googleit constructor.
+     * @param array $infos
+     * @param \TwitchBot\IrcConnect $client
+     */
+    public function __construct(array $infos, $client)
+    {
+        $this->moduleConstructor($infos, $client);
+    }
+
+    /**
+     * @param \TwitchBot\Message $message
+     */
+    public function onMessage($message)
+    {
+        if(substr($message->getMessage(), 0, 7) == '!google'){
+
+            $userToPing = explode(' ', $message->getMessage())[1];
+
+            $request = substr($message->getMessage(), 9 + strlen($userToPing));
+            $url = "http://www.letmegooglethat.com/?q=" . urlencode($request);
+
+            $message = sprintf($this->getConfig('message'), $userToPing, $url);
+
+            $this->getClient()->sendMessage($message);
+        }
+    }
+}
